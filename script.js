43e53b5ea8104468265c38f5cde7fcda265ed6c0
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-16 15:25:58 -0600

    278 JS npm scripts

--- script.js
+++ script.js
@@ -89,4 +89,46 @@ export.addTocart = function (product, quantity) {
   //IMPORT
   const { addTocart } = require('./shoppingCart.js')
   */
-//////////// COMMON JS MODULES///////////
+//////////// INTRODUCTION TO NPM ///////////
+
+// import cloneDeep from 'lodash/cloneDeep';
+
+// const state = {
+//   cart: [
+//     { product: 'bread', quantity: 5 },
+//     { product: 'pizza', quantity: 5 },
+//   ],
+//   user: { loggedIn: true },
+// };
+// const stateClone = Object.assign({}, state);
+// const stateDeepClone = cloneDeep(state);
+
+// state.user.loggedIn = false;
+// console.log(stateClone);
+
+// console.log(stateDeepClone);
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+// class Person {
+//   #greeting = 'Hey';
+//   constructor(name) {
+//     this.name = name;
+//     console.log(`${this.#greeting}, ${this.name}`);
+//   }
+// }
+// const jonas = new Person('Jonas');
+
+// console.log('Jonas' ?? null);
+
+// console.log(cart.find(el => el.quantity >= 2));
+// Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+// // import 'core-js/stable/array/find';
+// // import 'core-js/stable/promise';
+
+// // Polifilling async functions
+// import 'regenerator-runtime/runtime';
